54f68f00491ef9752bcc33486bcfa8c3c9bef41f
Ecem Karkin <s.ecem_er@hotmail.com>
2020-02-26 22:12:40 +0300

    Add and implement locators to Home and Login pages

---
+++ Locators/locators.py
@@ -0,0 +1,12 @@
+class Locators():
+
+    # Login page objects
+
+    username_textbox_id = "txtUsername"
+    password_textbox_id = "txtPassword"
+    login_button_id = "btnLogin"
+
+    # Home Page objects
+
+    welcome_link_id = "welcome"
+    logout_link_text_id = "Logout"
